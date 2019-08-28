6fdd466040e09aaf0e39acdd322a80830a0002cc
Daniel Cifuentes <danixbox1313@gmail.com>
2019-08-28 09:56:40 -0500

    Implemented tests for getBlueprint and getBlueprintsByAuthor

--- src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -11,12 +11,17 @@ import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author hcadavid
  */
+@Component("bpp")
 public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
 
     private final Map<Tuple<String,String>,Blueprint> blueprints=new HashMap<>();
@@ -43,6 +48,22 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
     public Blueprint getBlueprint(String author, String bprintname) throws BlueprintNotFoundException {
         return blueprints.get(new Tuple<>(author, bprintname));
     }
+
+    @Override
+    public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException {
+        if (!blueprints.containsKey(author)) {
+            throw new BlueprintNotFoundException("the given author doesn´t exist");
+        } else {
+            Set<Blueprint> bprints = new HashSet<Blueprint>();
+            for(Blueprint b: blueprints.values())
+                if(b.getAuthor().equals(author))
+                    bprints.add(b);
+            return bprints;
+        }
+        
+    }
+
+    
 
     
     
--- src/test/java/edu/eci/arsw/blueprints/test/persistence/impl/InMemoryPersistenceTest.java
+++ src/test/java/edu/eci/arsw/blueprints/test/persistence/impl/InMemoryPersistenceTest.java
@@ -10,6 +10,8 @@ import edu.eci.arsw.blueprints.model.Point;
 import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
 import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
 import edu.eci.arsw.blueprints.persistence.impl.InMemoryBlueprintPersistence;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.Test;
@@ -67,6 +69,62 @@ public class InMemoryPersistenceTest {
         }
                 
         
+    }
+    
+    @Test
+    public void getBlueprintTest() throws BlueprintPersistenceException, BlueprintNotFoundException{
+        InMemoryBlueprintPersistence ibpp= new InMemoryBlueprintPersistence();
+        
+        Point[] pts= new Point[]{new Point(0,0),new Point(10,10)};
+        
+        Blueprint bp=new Blueprint("john", "thepaint", pts);
+        ibpp.saveBlueprint(bp);
+        
+        Blueprint bp2= new Blueprint("amy", "thepaint", pts);
+        ibpp.saveBlueprint(bp2);
+        
+        assertEquals("getBlueprint doesn't get accurate blueprint if the name is the same as another", bp, ibpp.getBlueprint("john", "thepaint"));
+        
+        try{
+            ibpp.getBlueprint("arthur", "thepaint");
+            fail("an exception was expected because the author doesn´t exist");
+        } catch(BlueprintNotFoundException e){
+            
+        }
+    }
+    
+    @Test
+    public void getBlueprintsByAuthorTest() throws BlueprintPersistenceException, BlueprintNotFoundException{
+        InMemoryBlueprintPersistence ibpp= new InMemoryBlueprintPersistence();
+        
+        Point[] pts= new Point[]{new Point(0,0),new Point(10,10)};
+        Set<Blueprint> bprints = new HashSet<>();
+        Blueprint bp=new Blueprint("john", "thepaint", pts);
+        ibpp.saveBlueprint(bp);
+        bprints.add(bp);
+        
+        Blueprint bp2= new Blueprint("amy", "thepaint", pts);
+        ibpp.saveBlueprint(bp2);
+        bprints.add(bp2);
+        
+        Blueprint bp3 =new Blueprint("john", "thepaint 2: electric boogaloo", pts);
+        ibpp.saveBlueprint(bp3);
+        bprints.add(bp3);
+        
+        Blueprint bp4= new Blueprint("amy", "thepaint2", pts);
+        ibpp.saveBlueprint(bp4);
+        bprints.add(bp4);
+        
+        Blueprint bp5=new Blueprint("john", "the revenge of thepaint", pts);
+        ibpp.saveBlueprint(bp5);
+        bprints.add(bp5);
+        
+        Blueprint bp6= new Blueprint("amy", "thepaint3", pts);
+        ibpp.saveBlueprint(bp6);
+        bprints.add(bp6);
+        
+        for(Blueprint b:ibpp.getBlueprintsByAuthor("john"))
+            assertEquals("every author should have the same name",b.getAuthor(), "john");
     }
 
 
